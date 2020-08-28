d01133e99ea84b566af3d0b9c9b56ae9551b208e
Alexander <alexandercos14@gmail.com>
2020-08-27 22:36:52 -0500

    first commit, header and basic structure

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./main.css";
+
+export default () => {
+  return (
+    <div className="header">
+      <Link to="/">EMOJIS</Link>
+    </div>
+  );
+};
